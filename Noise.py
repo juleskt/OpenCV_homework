7dd16d157a8fe665558ca94ca9b0e38aeab77c5a
Julian Trinh <jtrinh@vmware.com>
2017-11-06 14:57:27 -0500

    Fixed Exercise Three

--- Noise.py
+++ Noise.py
@@ -1,24 +1,23 @@
 import cv2
 import sys
+import numpy as np
  
 def addGaussianNoise(img, mean, sigma):
     newImg = img.copy()
-    noiseImage = img.copy()
-    rng = cv2.CV_RNG().fill(noiseImage, 1, mean, sigma)
-    return newImg
+    noiseImage = np.zeros(img.shape, np.uint8)
+    cv2.randn(noiseImage, mean, sigma)
+    return cv2.add(newImg, noiseImage)
 
 def addSaltAndPepperNoise(src, pa, pb):
-    img = src.copy()
-    amountOne = img.rows * img.cols * pa
-    amountTwo = img.rows * img.cols * pb
+    rnd = np.random.rand(src.shape[0], src.shape[1])
+    noiseImage = src.copy()
 
-    for x in range(0, amountOne):
-        img[(cv2.CV_RNG().uniform(0, img.rows)), cv2.CV_RNG().uniform(0, img.cols))] = 0
+    noiseImage[rnd < pa] = 0
+    noiseImage[rnd > 1 - pa] = 1
 
-    for x in range(0, amountTwo):
-        img[(cv2.CV_RNG().uniform(0, img.rows)), cv2.CV_RNG().uniform(0, img.cols))] =0
+    noiseImage[rnd > 1 - pb] = 255
 
-    return img
+    return noiseImage
 
 src = cv2.imread(sys.argv[1], 0)
 cv2.imshow("Original Image", src)
@@ -27,23 +26,25 @@ noisyImg = addGaussianNoise(src, 0, 50)
 cv2.imshow("Gaussian Noise", noisyImg)
 
 noiseDst = cv2.blur(noisyImg.copy(),(3,3))
-imshow("Box filter", noiseDst)
+cv2.imshow("GBox filter", noiseDst)
 
 noiseDst1 = cv2.GaussianBlur(noisyImg.copy(), (3,3), 1.5)
-imshow("Gaussian Filter", noiseDst1)
+cv2.imshow("GGaussian Filter", noiseDst1)
 
 noiseDst2 = cv2.medianBlur(noisyImg.copy(), 3)
-imshow("Median Filter", noiseDst2)
+cv2.imshow("GMedian Filter", noiseDst2)
 
 
 noisyImg2 = addSaltAndPepperNoise(src, .01, .01)
-imshow("Salt and Pepper Noise", noisyImg2)
+cv2.imshow("Salt and Pepper Noise", noisyImg2)
 
 noiseDst = cv2.blur(noisyImg2.copy(),(3,3))
-imshow("Box filter", noiseDst)
+cv2.imshow("Box filter", noiseDst)
 
 noiseDst1 = cv2.GaussianBlur(noisyImg2.copy(), (3,3), 1.5)
-imshow("Gaussian Filter", noiseDst1)
+cv2.imshow("Gaussian Filter", noiseDst1)
 
 noiseDst2 = cv2.medianBlur(noisyImg2.copy(), 3)
-imshow("Median Filter", noiseDst2)
+cv2.imshow("Median Filter", noiseDst2)
+
+cv2.waitKey(0)
